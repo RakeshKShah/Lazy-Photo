354758bc39aed7a61d301c7048f87766823d1f71
Rakesh <rakesh.shah.iiita@gmail.com>
2015-02-14 16:55:47 +0530

    Carousel construction

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 var albumarts = [
- 'http://upload.wikimedia.org/wikipedia/en/2/29/Soulcrusher_Album_Art.jpg',
- 'http://th06.deviantart.net/fs71/PRE/i/2013/361/7/5/lights_album_art_by_bonvallet-d6ylvvi.png',
- 'http://www.whudat.de/images/graffiti_streetart_album_covers_02.jpg',
- 'http://www.smashingmagazine.com/images/music-cd-covers/64.jpg',
- 'http://style.mtv.com//wp-content/uploads/style/2012/10/rihanna-side-effects-album-art.jpg',
- 'http://cdn3.pitchfork.com/news/50068/3f8a6f3b.jpg',
- 'http://assets.nydailynews.com/polopoly_fs/1.1439189!/img/httpImage/image.jpg_gen/derivatives/article_970/drake29f-1-web.jpg'
+  'http://upload.wikimedia.org/wikipedia/en/2/29/Soulcrusher_Album_Art.jpg',
+  'http://th06.deviantart.net/fs71/PRE/i/2013/361/7/5/lights_album_art_by_bonvallet-d6ylvvi.png',
+  'http://www.whudat.de/images/graffiti_streetart_album_covers_02.jpg',
+  'http://www.smashingmagazine.com/images/music-cd-covers/64.jpg',
+  'http://style.mtv.com//wp-content/uploads/style/2012/10/rihanna-side-effects-album-art.jpg',
+  'http://cdn3.pitchfork.com/news/50068/3f8a6f3b.jpg',
+  'http://assets.nydailynews.com/polopoly_fs/1.1439189!/img/httpImage/image.jpg_gen/derivatives/article_970/drake29f-1-web.jpg'
 ];
 
 var getImageElm = function(src) {
@@ -21,25 +21,36 @@ var ImgFrame = {
     srcElm : '',
 
     loadImage: function(imgSrc, idx) {
-          this.img[idx] = $(getImageElm(imgSrc));
-          this.img[idx].load(this._onload.bind(this));
-        },
+      this.img[idx] = $(getImageElm(imgSrc));
+      this.img[idx].load(this._onload.bind(this));
+    },
 
     _onload: function() {
-          this.srcElm.append(this.img[this.index]);
-          this.index++;
-        },
+      this.srcElm.append(this.img[this.index]);
+      this.index++;
+    },
 
     init : function(album) {
-          this.album = album;
-          this.srcElm = $('#image-display');
+      this.album = album;
+      this.srcElm = $('#image-display');
 
-          this.album.forEach(function(imgSrc, idx) {
-                  this.loadImage(imgSrc, idx);
-                }, this);
-        },
+      this.album.forEach(function(imgSrc, idx) {
+        this.loadImage(imgSrc, idx);
+      }, this);
+    },
+
+    loadNext : function() {
+      this.loadImage(albumarts[this.index], this.index);
+    },
+
+    loadPrevious : function() {
+      console.log('here in load previous');
+    },
 };
 
+
 $(document).ready(function() {
-   ImgFrame.init(albumarts);
+   var _albumarts = [];
+   _albumarts.push(albumarts[0]);
+   ImgFrame.init(_albumarts);
 });
